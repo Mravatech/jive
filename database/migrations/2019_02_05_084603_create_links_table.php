6090133546cae09c0cfdbf337cb012a94dea8f79
codeliter <hackzlord@gmail.com>
2019-02-05 10:14:16 +0100

    Created Table for Links and Links_log

---
+++ database/migrations/2019_02_05_084603_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->integer('event_id');
+            $table->integer('created_by');
+            $table->text('link');
+            $table->integer('visit_count');
+            $table->timestamps();
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ database/migrations/2019_02_05_085236_create_links_log_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links_log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->integer('event_id');
+            $table->integer('link_id');
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('link_id')->references('id')->on('links');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links_log');
+    }
+}
